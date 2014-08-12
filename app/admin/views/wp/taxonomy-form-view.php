595169adfb378d7f169d05f183ea088fb2159637
dkenzox <enzonicolorich@gmail.com>
2014-08-12 13:36:34 -0300

    Added validation for smartRules form

--- app/admin/views/wp/taxonomy-form-view.php
+++ app/admin/views/wp/taxonomy-form-view.php
@@ -4,13 +4,18 @@
 <?php \Maven\Core\UI\HtmlComponent::jSonComponent( 'CachedSmartTermOperators', $smartTermOperators ); ?>
 <div >	
 	<table class="form-table" ng-controller="TaxonomyController">
+		<tr>
+		<td colspan="2">
+			<alert type="alert" ng-hide="smartIsUnique">There should be no duplicate rules</alert>
+			<alert type="alert" ng-show="smartTermRules.length === 0 && isSmartTerm">There should be at least one rule</alert>
+		</td>
+		</tr>
 		<tr class="is-smart-rules-form">
 			<td>
 				<?php $lang->_e( 'Is Smart Category' ); ?>
 			</td>
 			<td>
-				<input type="checkbox" name="mvn_smart_rules[is_smart_term]" ng-model="isSmartTerm"
-					   ng-true-value="1" ng-false-value="0"/>
+				<input type="checkbox" name="mvn_smart_rules[is_smart_term]" ng-model="isSmartTerm" ng-change="checkForDuplicates()"/>
 			</td>
 		</tr>
 		<tr class="smart-rules-form" ng-if="isSmartTerm">
@@ -28,19 +33,19 @@
 							<button class="add-rule" ng-click="removeTermRule($index)">
 								X
 							</button>
-							<select class="add-rule" name="mvn_smart_rules[mvn_shop_term][smart_rules][{{$index}}][field]" ng-model="termRule.field" ng-options="i as value for (i,value) in smartTermFields">
+							<select class="add-rule" name="mvn_smart_rules[mvn_shop_term][smart_rules][{{$index}}][field]" ng-model="termRule.field" ng-options="i as value for (i,value) in smartTermFields" ng-change="checkForDuplicates()">
 							</select>
-							<select class="add-rule" name="mvn_smart_rules[mvn_shop_term][smart_rules][{{$index}}][operator]" ng-model="termRule.operator" ng-options="i as value for (i,value) in smartTermOperator">
+							<select class="add-rule" name="mvn_smart_rules[mvn_shop_term][smart_rules][{{$index}}][operator]" ng-model="termRule.operator" ng-options="i as value for (i,value) in smartTermOperator" ng-change="checkForDuplicates()">
 							</select>
-							<input type="text" ng-if="termRule.field && termRule.operator" name="mvn_smart_rules[mvn_shop_term][smart_rules][{{$index}}][rule]" class="col-sm-5" ng-model="termRule.rule"/>
+							<input type="text" ng-if="termRule.field && termRule.operator" name="mvn_smart_rules[mvn_shop_term][smart_rules][{{$index}}][rule]" class="col-sm-5" ng-model="termRule.rule" ng-blur="checkForDuplicates()"/>
 						</li>
 						<?php // if ( $smart_term_rules ):    ?>
 						<?php // foreach ( $smart_term_rules as $key => $smart_rule ):    ?>
-							<!--<li id="mvn-shop-smart-rule-////<?php // echo $key;              ?>" class="mvn-shop-smart-rule">-->
-								<!--<label><button id="remove-smart-rule-////<?php // echo $key;              ?>" class="button-secondary remove_smart_rule">X</button> <?php // $lang->_e( 'Rule' );             ?></label>-->
+							<!--<li id="mvn-shop-smart-rule-////<?php // echo $key;                        ?>" class="mvn-shop-smart-rule">-->
+								<!--<label><button id="remove-smart-rule-////<?php // echo $key;                        ?>" class="button-secondary remove_smart_rule">X</button> <?php // $lang->_e( 'Rule' );                       ?></label>-->
 						<?php // Mvn_Shop_Product_Taxonomies::dropdown_fields( array( 'selected' => $smart_rule[ 'field' ], 'name' => "mvn_shop_term[smart_rules][{$key}][field]", 'id' => "mvn-shop-category-smart-field-{$key}" ) );    ?>
 						<?php // Mvn_Shop_Product_Taxonomies::dropdown_operators( array( 'selected' => $smart_rule[ 'operator' ], 'name' => "mvn_shop_term[smart_rules][{$key}][operator]", 'id' => "mvn-shop-category-smart-operator-{$key}" ) );    ?>
-						<!--<input type="text" value="////<?php // echo esc_attr( $smart_rule[ 'rule' ] );              ?>" name="mvn_shop_term[smart_rules][<?php // echo $key;             ?>][rule]" />-->
+						<!--<input type="text" value="////<?php // echo esc_attr( $smart_rule[ 'rule' ] );                        ?>" name="mvn_shop_term[smart_rules][<?php // echo $key;                       ?>][rule]" />-->
 						<!--</li>-->
 						<?php // endforeach;    ?>
 						<?php // endif;    ?>
--- app/scripts/admin/controllers/taxonomy.js
+++ app/scripts/admin/controllers/taxonomy.js
@@ -1,6 +1,7 @@
 angular.module('mavenBooksApp').controller('TaxonomyController', ['$scope', '$http', function($scope, $http) {
 		$scope.smartTermRules = [];
 		$scope.smartOperator = {};
+		$scope.smartIsUnique = true;
 		$scope.isSmartTerm = CachedIsSmartTerm;
 		$scope.smartTermOperator = CachedSmartTermOperators;
 		$scope.smartTermFields = CachedSmartTermFields;
@@ -21,11 +22,42 @@ angular.module('mavenBooksApp').controller('TaxonomyController', ['$scope', '$ht
 				newRule.field = "meta:mvn_shop_regular_price";
 				newRule.operator = "is_greater_or_equal_than";
 				$scope.smartTermRules.push(newRule);
+				$scope.checkForDuplicates();
 			}
 		};
 		$scope.removeTermRule = function(idx) {
 			$scope.smartTermRules.splice(idx, 1);
+			$scope.checkForDuplicates();
 		};
 
+		$scope.checkForDuplicates = function() {
+			if ($scope.isSmartTerm) {
+				if ($scope.smartTermRules.length === 0) {
+					angular.element(document.getElementById('submit')).prop("disabled", "disabled");
+					return false;
+				}
+				for (var i = 0; i < $scope.smartTermRules.length; i++)
+				{
+					for (var j = 0; j < $scope.smartTermRules.length; j++)
+					{
+						if (i !== j)
+						{
+							if ($scope.smartTermRules[i].field === $scope.smartTermRules[j].field &&
+									$scope.smartTermRules[i].operator === $scope.smartTermRules[j].operator &&
+									$scope.smartTermRules[i].rule === $scope.smartTermRules[j].rule)
+							{
+								$scope.smartIsUnique = false;
+								angular.element(document.getElementById('submit')).prop("disabled", "disabled");
+								return false;
+							}
+						}
+					}
+				}
+
+			}
+			$scope.smartIsUnique = true;
+			angular.element(document.getElementById('submit')).removeProp("disabled");
+			return true;
+		};
 	}]);
  
