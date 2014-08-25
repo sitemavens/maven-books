87cf82bcdc654aa99a030a2edf9838d1e2adb689
Lucas Perez <lucas@dinkuminteractive.com>
2014-08-25 17:47:05 -0300

    Add book to wishlist

---
+++ app/core/domain/whishlist-item.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MavenBooks\Core\Domain;
+
+// Exit if accessed directly 
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+class WishlistItem extends \Maven\Core\Domain\WishlistItem {
+	
+	
+}
